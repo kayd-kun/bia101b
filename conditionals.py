1fb161635322e4d4dda2ac995398511fee9e246c
Kamal Acharya <kamalacharya.cst@rub.edu.bt>
2024-03-07 14:14:25 +0600

    check for positive and negative numbers

--- conditionals.py
+++ conditionals.py
@@ -1,36 +1,39 @@
-# Objective: a calculator application made using 
-# variables and if statements
+# # Objective: a calculator application made using 
+# # variables and if statements
 
-# STEPS
-# 1. get input from user --> DONE
+# # STEPS
+# # 1. get input from user --> DONE
 
-# 2. do calculation based on user input
-# 2.1 check what string did user typed
-# 2.2 if usser string == * then do multiplicatio and so on
+# # 2. do calculation based on user input
+# # 2.1 check what string did user typed
+# # 2.2 if usser string == * then do multiplicatio and so on
 
-# 3. give output to the user
+# # 3. give output to the user
 
-print('* for multipliocation')
-print('+ for addition')
-print('- for subtraction')
-print('/ for division')
+# print('* for multipliocation')
+# print('+ for addition')
+# print('- for subtraction')
+# print('/ for division')
 
-whatUserTyped = input()
+# whatUserTyped = input()
 
-print('User typed:', whatUserTyped)
-print('user input-type', type(whatUserTyped))
+# print('User typed:', whatUserTyped)
+# print('user input-type', type(whatUserTyped))
 
-print('-------------------')
-if whatUserTyped == "+":
-    print('Doing Addition')
-    if 'a' == 'b':
-        print('a is not b')
-    if 'b' == 'b':
-        print('b is b')
+# print('-------------------')
+# if whatUserTyped == "+":
+#     print('Doing Addition')
+#     if 'a' == 'b':
+#         print('a is not b')
+#     if 'b' == 'b':
+#         print('b is b')
 
-print('doing more addition')
+# print('doing more addition')
 
-if whatUserTyped == "-":
-    print('Doing Subtraction')
-    print('doing more subtraction')
+# if whatUserTyped == "-":
+#     print('Doing Subtraction')
+#     print('doing more subtraction')
 
+for = 'asdfkhj;asdf'
+
+print(if)
