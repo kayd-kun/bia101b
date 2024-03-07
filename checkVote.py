d592ee8b119072d5789f788bafbe98b5ebee9290
Kamal Acharya <kamalacharya.cst@rub.edu.bt>
2024-03-07 14:27:59 +0600

    check if user can vote

---
+++ checkVote.py
@@ -0,0 +1,28 @@
+
+# ! check if user can vote
+
+# ?Get user age from input
+# ?convert userinput(str) to int() to number
+# ?check if user can vote
+    # ?if else statement
+    # ?if above 18, print "You can vote"
+    # ?if below 18, print "You canot vote"
+
+
+# 1. Get user input
+userInput = input('Please type your age: ')
+
+# 2. Convert user input to number
+userAge = int(userInput)
+
+# 3. Check if user can vote
+if userAge >= 18:
+    print('You can vote')
+else:
+    print('You cannot vote')
+
+# 3. Check if user can vote
+if userAge > 18:
+    print('You can vote')
+elif userAge < 18:
+    print('You cannot vote')
